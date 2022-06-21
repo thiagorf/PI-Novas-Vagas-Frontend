21a6c9a550f45eee4bebf42620b2bbcac36af519
thiagorf <trfarciroli@gmail.com>
2022-06-21 16:27:27 -0300

    fix: bad  container placement

--- src/pages/Main/main.tsx
+++ src/pages/Main/main.tsx
@@ -1,12 +1,11 @@
 import { ContentList } from "../../components/Content";
 import { SideBar } from "../../components/SideBar";
-import { MainWrapper } from "./main-wrapper";
 
 export const Main = () => {
     return (
-        <MainWrapper>
+        <>
             <SideBar />
             <ContentList />
-        </MainWrapper>
+        </>
     );
 };
